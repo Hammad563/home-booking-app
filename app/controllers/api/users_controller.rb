b89e8d4bdeefaa5a87ccec0490051090684791ab
Hammad <61918235+Hammad563@users.noreply.github.com>
2022-08-03 22:04:39 -0400

    intial commit with header and other components

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+
+module api
+    class UsersController < ApplicationController
+        
+        def show 
+            user = User.find(params[:id])
+            
+            respond_to do |format|
+                format.json do
+                    render json: user.to_json, status: 200
+                end
+            end
+        rescue ActiveRecord::RecordNotFound => e 
+            respond_to do |format|
+                format.json do
+                    render json: {error: e.message }.to_json, status: 404
+                end
+            end
+        end
+
+
+
+    end
+end
