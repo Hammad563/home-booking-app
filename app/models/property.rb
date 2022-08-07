38258547b024013696367df3bb674a93b029381b
Hammad <61918235+Hammad563@users.noreply.github.com>
2022-08-07 19:18:45 -0400

    property modal images and enhancement

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,10 +7,15 @@ class Property < ApplicationRecord
     validates :country, presence: true
     validates :address_1, presence: true
     monetize :price_cents, allow_nil: true
+    has_many_attached :images, dependent: :destroy
     geocoded_by :address
     after_validation :geocode, if: -> { latitude.blank? && longitude.blank? }
     def address
         # [address_1, address_2, city, state, country].compact.join(', ')
         [state, country].compact.join(', ')
     end
+    def default_image
+        images.first
+    end
+
 end
