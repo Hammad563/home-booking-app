baa14fd79f5ce9a82ff55d0e0573c43a56a0ae5b
Hammad <61918235+Hammad563@users.noreply.github.com>
2022-08-11 20:20:56 -0400

    added reservation table and show page for properties

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,20 +1,22 @@
 import { Controller } from '@hotwired/stimulus'
-import { enter, leave, toggle } from 'el-transition'
+import { leave, toggle } from 'el-transition'
 
 export default class extends Controller {
   static targets = ['openUserMenu', 'userAuthLink'];
 
   connect() {
     this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
-    this.userAuthLinkTargets.forEach( (link) => {
+
+    this.userAuthLinkTargets.forEach((link) => {
       link.addEventListener('click', (e) => {
-        e.preventDefault()
-        document.getElementById('modal-trigger').click()
-      })
-    })
+        e.preventDefault();
+        document.getElementById('user-auth-modal-trigger').click();
+      });
+    });
   }
 
   toggleDropdownMenu() {
     toggle(document.getElementById('menu-dropdown-items'));
   }
+
 }
